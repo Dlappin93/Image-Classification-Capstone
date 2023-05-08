be886ca1c33ff28294309e645e5c76d97ad0e8db
Dlappin93 <dlappin93@gmail.com>
2023-05-08 19:36:12 -0400

    app updates

--- oregon_species_app.py
+++ oregon_species_app.py
@@ -3,35 +3,7 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 from PIL import Image
-import requests
 
-
-# Load the pre-trained model
-model = tf.keras.models.load_model('appmodel.h5')
-
-# Compile the model
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
-# Define the class names
-class_names = ['bald_eagle', 'black_bear', 'bobcat', 'canada_lynx', 'columbian_black-tailed_deer', 'cougar',
-               'coyote', 'deer', 'elk', 'gray_fox', 'gray_wolf', 'mountain_beaver', 'nutria', 'raccoon', 'raven',
-               'red_fox', 'ringtail', 'sea_lions', 'seals', 'virginia_opossum']
-
-# Function to preprocess the image
-def preprocess_image(image):
-    img = image.resize((224, 224))  # Resize the image to match the input size of the model
-    img = tf.keras.preprocessing.image.img_to_array(img)
-    img = np.expand_dims(img, axis=0)
-    img = tf.keras.applications.mobilenet_v2.preprocess_input(img)
-    return img
-
-# Function to make predictions
-def predict(image):
-    processed_image = preprocess_image(image)
-    prediction = model.predict(processed_image)
-    predicted_class = np.argmax(prediction)
-    confidence = prediction[0][predicted_class]
-    return class_names[predicted_class], confidence
 
 # Streamlit app code
 st.title("Image Classifier")
