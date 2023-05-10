7bdb92d532df04930585cbfc80930555bc8b74f5
Dlappin93 <dlappin93@gmail.com>
2023-05-10 11:06:52 -0400

    final app adjustments and code markdown

--- oregon_species_app.py
+++ oregon_species_app.py
@@ -1,17 +1,20 @@
+
+#import relevant packages
 import streamlit as st
 import tensorflow as tf
 import numpy as np
 from PIL import Image
 from tensorflow.keras.applications.resnet50 import preprocess_input
 
+#define the class labels that the model was trained with
 class_labels = ['Bald Eagle', 'Black Bear', 'Bobcat', 'Canada Lynx', 'Columbian Black-tailed Deer', 'Cougar',
                 'Coyote', 'Deer', 'Elk', 'Gray Fox', 'Gray Wolf', 'Mountain Beaver', 'Nutria', 'Raccoon', 'Raven',
                 'Red Fox', 'Ringtail', 'Sea Lions', 'Seals', 'Virginia Opossum']
 
-# Load the model
+#load model
 model = tf.keras.models.load_model('appmodel.h5')
 
-
+#preprocess image so the model accepts the image dimensions
 def preprocess_image(image):
     img = Image.open(image)
     img = img.resize((224, 224))
@@ -20,7 +23,7 @@ def preprocess_image(image):
     img = np.expand_dims(img, axis=0)
     return img
 
-
+#Use preprocessed image and loaded model to predict class
 def predict_image(image):
     img = preprocess_image(image)
     predictions = model.predict(img)
@@ -31,18 +34,6 @@ def predict_image(image):
 #Define App Text and Styling
 def main():
 
-    st.markdown(
-        """
-        <style>
-        body {
-            background-color: #F5F5F5;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
-
     # Add title and purpose section
     st.title("Oregon Wildlife Classification Using Transfer Learning with Fine-Tuned MobileNetV2 Pretrained Model")
 
@@ -52,7 +43,7 @@ def main():
     st.markdown("Date: May 10, 2023")
     st.markdown("---")
 
-
+#Project information section
     st.subheader("Project and Application Info:")
     st.markdown("This application performs image classification on 20 different species classes from Oregon. "
                 "The predictions are generated using the MobileNetV2 pretrained model that has been fine-tuned to "
@@ -60,13 +51,14 @@ def main():
                 "\n\n "
                 "This application is a final addition to a larger project on image classification using CNN models "
                 "in Keras/Tensorflow. The purpose was to try and identify different species so that the same code could "
-                "be trained using larger volumes of trail camera footage. In the Future I will be adapting the project"
+                "be trained using larger volumes of trail camera footage. In the future I will be adapting the project "
                 "to fine tune the model to trail camera footage for species identification and quantification"
                 "The project in its entirety, as well as the code and requirements for this application"
                 "can be found on my github: https://github.com/Dlappin93/Image-Classification-Capstone")
     st.markdown("---")
     st.markdown("\n\n")
 
+#include a table to show all species classes accepted by application
     st.markdown("### Species Classes")
     st.markdown("Note that while you can use any image in the app, the model is only trained to recognize the following classes:")
     table_html = "<table>"
@@ -83,13 +75,14 @@ def main():
     st.markdown("\n\n")
     st.markdown("\n\n")
     st.markdown("---")
-    
+
+#markdown title for the app
     st.subheader("Try It Out!:")
     st.markdown("\n\n")
     st.markdown("\n\n")
 
 
-    # Upload and display the image
+# Upload and display the image
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
